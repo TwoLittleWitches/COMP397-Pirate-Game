f66c03108c92525e671e2409e32f416b5b3cc706
VincentChen8 <vincentchenofficial@gmail.com>
2024-04-01 17:42:46 -0400

    Added in enhanced footsteps and jumping sounds
    Also reworked a bit on the jump animation to align it more with the new jump sound effects

---
+++ Assets/_Scripts/FootSteps.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+
+public class FootSteps : MonoBehaviour
+{
+    [SerializeField]
+    private AudioClip[] sandClips;
+    [SerializeField]
+    private AudioClip[] woodClips;
+    [SerializeField]
+    private AudioClip[] stoneClips;
+    [SerializeField]
+    private AudioClip[] otherClips;
+
+    private AudioSource footstepAudioSource;
+    private TerrainDetector terrainDetector;
+
+    private void Awake()
+    {
+        footstepAudioSource = transform.Find("FootstepAudioSource").GetComponent<AudioSource>();
+        terrainDetector = new TerrainDetector();
+    }
+
+    private void Step()
+    {
+        int terrainTextureIndex = terrainDetector.GetActiveTerrainTextureIdx(transform.position);
+        AudioClip clip = GetRandomClip(terrainTextureIndex);
+        if (clip != null)
+        {
+            footstepAudioSource.PlayOneShot(clip);
+        }
+    }
+
+    private AudioClip GetRandomClip(int terrainTextureIndex)
+    {
+        switch (terrainTextureIndex)
+        {
+            case 0:
+                return sandClips[Random.Range(0, sandClips.Length)];
+            case 1:
+                return woodClips[Random.Range(0, woodClips.Length)];
+            case 2:
+                return stoneClips[Random.Range(0, stoneClips.Length)];
+            case 3:
+            default:
+                return otherClips[Random.Range(0, otherClips.Length)];
+        }
+    }
+}
---
+++ Assets/_Scripts/TerrainMaterial.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+internal class TerrainMaterial
+{
+    internal void OnPlayerEnter()
+    {
+        throw new NotImplementedException();
+    }
+
+    internal void OnPlayerExit()
+    {
+        throw new NotImplementedException();
+    }
+}
